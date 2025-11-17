9b15309a846322b8022627fd7173b7fe54852045
Christian Bruni <christianbruni131@gmail.com>
2025-11-17 16:14:27 +0100

    ~ definizione struttura main + kernel.cuh che conterrà il kernel di   aggiornamento

--- main.cu
+++ main.cu
@@ -1,3 +1,51 @@
 #define step 0
 #define nStep 10000
-#define gridRows 
+#define gridRows 1 << 8
+#define gridCols 1 << 12
+#define nHotTopRows 2
+#define nHotBottomRows 2
+#define initialHotTemperature 20
+#define fieldWidth 5
+
+#define outFilePrefix "Temperature"
+#define outFileExtension ".dat"
+
+int main()
+{
+    size_t size= (size_t)gridRows*gridCols*sizeof(float);
+    float *matPrev = (float *) malloc(size);
+    float *matNext = (float *) malloc(size);
+
+    float *deviceMatPrev= NULL;
+    float *deviceMatNext= NULL;
+
+    cudaMallocManaged((void**) &deviceMatPrev,size);
+    cudaMallocManaged((void**) &deviceMatNext,size);
+
+    // TODO: initMat
+
+    // TODO: salvataggio su file iniziale
+
+    // inizio timer CUDA
+
+    for (size_t i=1; i <=nStep; i++)
+    {
+        // TODO  update region
+        // TODO swap buffer
+    }
+
+    // fine timer CUDA
+
+    // stampa tempo
+
+    // salva su file finale
+
+    // delete
+
+
+    
+
+
+
+    return 0;
+}
