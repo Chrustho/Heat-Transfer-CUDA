0578015a84481a784fbc103218ca2fb845be143d
Kiriaevi <fra977502@gmail.com>
2025-11-17 21:28:39 +0100

    + corretto BUG in cui l'operatore '<<' non era prioritario + separato metodo per il run del kernel e degli eventi per misurazione performance in un metodo a parte + integrato esempio di print in un commento

--- main.cu
+++ main.cu
@@ -1,7 +1,7 @@
 #define step 0
 #define nStep 10000
-#define gridRows 1 << 8
-#define gridCols 1 << 12
+#define gridRows (1 << 8)
+#define gridCols (1 << 12)
 #define nHotTopRows 2
 #define nHotBottomRows 2
 #define initialHotTemperature 20
@@ -47,6 +47,29 @@ void copyGPU(float *to, float *from, int size, cudaMemcpyKind op) {
         exit(G_COPY_ERROR);
     }
 }
+/**
+* Metodo per l'esecuzione del kernel e la valutazione in ms del tempo di esecuzione.
+* Questo esegue una simulazione parallela per un numero di passi pari a nStep.
+*/
+void runKernel(dim3 blockDim, dim3 gridDim, int dim1, int dim2) {
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        cudaEventRecord(start,0);
+
+        for (size_t i = 0; i < nStep; i++)
+        {
+            // chiama kernel
+        }
+
+        cudaEventRecord(stop,0);
+        cudaEventSynchronize(stop);
+
+        float ms=0.0f;
+        cudaEventElapsedTime(&ms, start, stop);
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+}
 int main()
 {
     size_t size= (size_t)gridRows*gridCols*sizeof(float);
@@ -58,14 +81,10 @@ int main()
 
     mallocGPU(&deviceMatPrev, size, true);
     mallocGPU(&deviceMatNext, size, true);
-
-    
-    int threadsInit = 1024;
-    int blocksInit = (int)(((size/ sizeof(float)) + threadsInit - 1) / threadsInit);
-
-    initTemperature<<<blocksInit,threadsInit>>>(deviceMatPrev, gridRows, gridCols, initialHotTemperature, nHotTopRows,nHotBottomRows);
-    cudaDeviceSynchronize();
-
+    //int threadsInit = 1024;
+    //int blocksInit = (int)(((size/ sizeof(float)) + threadsInit - 1) / threadsInit);
+    //initTemperature<<<blocksInit,threadsInit>>>(deviceMatPrev, gridRows, gridCols, initialHotTemperature, nHotTopRows,nHotBottomRows);
+    //cudaDeviceSynchronize();
     //salva su file
 
     int dims[]={8,16,32};
@@ -85,29 +104,12 @@ int main()
             dim3 blockDim(dim1, dim2);
             dim3 gridDim((gridCols + dim1 - 1) / dim1, 
                          (gridRows + dim2 - 1) / dim2);
+           initTemperature<<<gridDim,blockDim>>>(deviceMatPrev, gridRows, gridCols, initialHotTemperature, nHotTopRows,nHotBottomRows);
+           cudaDeviceSynchronize();
+           //printMatrix(deviceMatPrev, gridCols, gridRows); 
+           // salva su file
 
-            initTemperature<<<gridDim,blockDim>>>(deviceMatPrev, gridRows, gridCols, initialHotTemperature, nHotTopRows,nHotBottomRows);
-            cudaDeviceSynchronize();
-
-            // salva su file
-
-            cudaEvent_t start, stop;
-            cudaEventCreate(&start);
-            cudaEventCreate(&stop);
-            cudaEventRecord(start,0);
-
-            for (size_t i = 0; i < nStep; i++)
-            {
-                // chiama kernel
-            }
-
-            cudaEventRecord(stop,0);
-            cudaEventSynchronize(stop);
-
-            float ms=0.0f;
-            cudaEventElapsedTime(&ms, start, stop);
-            cudaEventDestroy(start);
-            cudaEventDestroy(stop);
+            //runKernel(blockDim, gridDim, dim1, dim2);
             
             // salvo su file
         }
